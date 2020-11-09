32f427fc8d918bc3e229f29e9ff56f8a90f0ce1b
Malu <mbumbangisila@gmail.com>
2020-11-09 13:00:03 +0100

    Authentification

--- routes/api.php
+++ routes/api.php
@@ -25,3 +25,18 @@ Route::resource('/service_evacuation', 'ServiceEvController');
 Route::resource('/fonction', 'FonctionController');
 Route::resource('/personne', 'PersonneController');
 Route::post('/agent', 'PersonneController@AddAgent');
+
+Route::group(
+    [
+        'middleware' => 'api',
+        'prefix' => 'auth'
+
+    ],
+    function ($router) {
+        Route::post('login', 'AuthController@login');
+        Route::post('register', 'AuthController@register');
+        Route::post('logout', 'AuthController@logout');
+        Route::post('refresh', 'AuthController@refresh');
+        Route::get('user-profile', 'AuthController@userProfile');
+    }
+);
